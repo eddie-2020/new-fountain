4664449a8c522e437f3ad39bfdaafc9a13958bac
eddie-tomisin <oluyaratosin123@gmail.com>
2024-04-16 14:08:15 -0700

    update core pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="text-center mt-[70px] mb-[70px]">
-      <div>
-        <h1 className="text-[20px] md:text-[30px]">Contact New Fountain</h1>
+    <div className="text-center mt-[100px] mb-[70px]">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-[20px] md:text-[30px]">Contact New Fountain</h1>
+        <div className="mt-2 w-10 h-1 rounded-lg bg-green-400"></div>
       </div>
     </div>
   );
